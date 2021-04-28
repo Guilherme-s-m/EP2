1b943ce3fe36463a2c3ff436115539e443ccaacc
LuanCintra <luancintra1234@outlook.com>
2021-04-28 11:09:12 -0300

    Biblioteca e Funções definidas

--- programa-ep2.py
+++ programa-ep2.py
@@ -1,1 +1,8 @@
-input("nome?")
+#importar biblioteca de sortear
+import random
+#função cria baralho
+def criar_baralho():
+#função extrair naipe da carta
+def extrair_naipe(carta):
+#função extrair valor da carta
+def extrair_valor(carta):
