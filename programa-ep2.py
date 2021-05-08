1bbab5a846af07b746ec6277bf4704e9adfd27bd
LuanCintra <luancintra1234@outlook.com>
2021-05-07 21:03:40 -0300

    cria baralho

--- programa-ep2.py
+++ programa-ep2.py
@@ -1,8 +1,21 @@
 #importar biblioteca de sortear
 import random
 #função cria baralho
-def criar_baralho():
-#função extrair naipe da carta
-def extrair_naipe(carta):
+def cria_baralho():
+    carta= ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+    naipe= ["♠","♥","♦","♣"]
+    baralho=[]
+    for n in naipe:
+        for c in carta:
+            carta= c+n
+            baralho.append(carta)
+    return baralho
+
+print cria_baralho()
+print baralho
+
+
+"""#função extrair naipe da carta
+def extrai_naipe():
 #função extrair valor da carta
-def extrair_valor(carta):
+def extrair_valor():"""
