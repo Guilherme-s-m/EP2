b1c17d1534f5c9af5c3e8c9e5f7ec43b4b3c32a4
LuanCintra <luancintra1234@outlook.com>
2021-05-07 23:32:26 -0300

    ...

--- lista_movimentos_possiveis.py
+++ lista_movimentos_possiveis.py
@@ -1,5 +1,3 @@
-#importar biblioteca de sortear
-import random
 #Função cria baralho
 def cria_baralho():
     carta= ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
@@ -15,7 +13,7 @@ def extrai_naipe(x):
     y = x[-1]
     return y
 
-#Função extrai valor da carta
+#Função extrai valor
 def extrai_valor(x):
     x = x[:-1] + ""
     return x
@@ -34,12 +32,3 @@ def lista_movimentos_possiveis(s,p):
     elif extrai_valor(s[p]) == extrai_valor(s[p-3]) and (p-3)>=0:
         lista_mov.append(3)
     return lista_mov
-
-
-#Função empilha
-def empilha(x,y,z):
-    x.remove(x[z]) 
-    x.insert(x[z],x[y])
-    x.remove(x[y])
-    return x
-print(empilha(['6♥', 'J♥', '9♣', '9♥'],1,0))
