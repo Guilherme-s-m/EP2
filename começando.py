a475446356f06e75517fcac39a8b5d0bf33b559b
Guilherme <guilhermesm9@al.insper.edu.br>
2021-05-07 21:28:27 -0300

    cria baralho pronta

--- começando.py
+++ começando.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 print('salve família')
-=======
-
->>>>>>> cb1c11e0ded672d14ad39c36b80d632dabdbaf04
--- programa-ep2.py
+++ programa-ep2.py
@@ -2,18 +2,18 @@
 import random
 #Função cria baralho
 def cria_baralho():
-    carta= ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+    carta= ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
     naipe= ["♠","♥","♦","♣"]
     baralho=[]
     for n in naipe:
         for c in carta:
-            carta= c+n
-            baralho.append(carta)
+            baralho.append(c+n)
     return baralho
+print(cria_baralho())
+print()
 
-
-Função extrai naipe
-  def extrai_naipe(x):
+#Função extrai naipe
+def extrai_naipe(x):
     y = x[-1]
     return y
 
