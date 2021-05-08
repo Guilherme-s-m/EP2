8cadb66a74220e24b68295bdcc1f96dff988cf36
LuanCintra <luancintra1234@outlook.com>
2021-05-07 23:02:11 -0300

    extrai naipe função

---
+++ extrai_naipe.py
@@ -0,0 +1,14 @@
+#Função cria baralho
+def cria_baralho():
+    carta= ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    naipe= ["♠","♥","♦","♣"]
+    baralho=[]
+    for n in naipe:
+        for c in carta:
+            baralho.append(c+n)
+    return baralho
+
+#Função extrai naipe
+def extrai_naipe(x):
+    y = x[-1]
+    return y
