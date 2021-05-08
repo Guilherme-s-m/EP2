ccd09514c59ba1fa96f50ecce2d22faacf420718
Guilherme <guilhermesm9@al.insper.edu.br>
2021-05-07 23:41:48 -0300

    tudo até agora

--- programa-ep2.py
+++ programa-ep2.py
@@ -17,12 +17,27 @@ def extrai_naipe(x):
     return y
 
 #Função extrai valor da carta
-def extrai_valor(x):
-    x = x[:-1] + ""
-    return x
+def extrai_valor(v):
+    v = v[:-1] + ""
+    return v
+
+#Função lista de movimentos possíveis
+def lista_movimentos_possiveis(s,p):
+    lista_mov= []
+    if p == 0:
+        return []
+    if extrai_naipe(s[p]) == extrai_naipe(s[p-1]):
+        lista_mov.append(1)
+    elif extrai_valor (s[p]) == extrai_valor(s[p-1]):
+        lista_mov.append(1)
+    if extrai_naipe(s[p]) == extrai_naipe(s[p-3]) and (p-3) >=0:
+        lista_mov.append(3)
+    elif extrai_valor(s[p]) == extrai_valor(s[p-3]) and (p-3)>=0:
+        lista_mov.append(3)
+    return lista_mov
 
 #Função empilha
-def empilha(x,y,z):
-    x[z] = x[y]
-    del(x[y])
-    return x
+def empilha(m,y,z):
+    m[z] = m[y]
+    del(m[y])
+    return m
