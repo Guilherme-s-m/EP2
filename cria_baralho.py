85273a60a54e26c52ef037e708e27e9eddec9fa8
LuanCintra <luancintra1234@outlook.com>
2021-05-07 21:43:08 -0300

    Função Cria baralho

---
+++ cria_baralho.py
@@ -0,0 +1,11 @@
+#Função cria baralho
+def cria_baralho():
+    carta= ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    naipe= ["♠","♥","♦","♣"]
+    baralho=[]
+    for n in naipe:
+        for c in carta:
+            baralho.append(c+n)
+    return baralho
+
+print (cria_baralho())       
