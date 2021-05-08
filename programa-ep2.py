f40b30534b05c66dfa4a8390899a1583c8c182e6
Guilherme-s-m <81168760+Guilherme-s-m@users.noreply.github.com>
2021-05-07 21:01:35 -0300

    Update programa-ep2.py

--- programa-ep2.py
+++ programa-ep2.py
@@ -4,5 +4,8 @@ import random
 def criar_baralho():
 #função extrair naipe da carta
 def extrair_naipe(carta):
+  def extrai_naipe(x):
+    y = x[-1]
+    return y
 #função extrair valor da carta
 def extrair_valor(carta):
